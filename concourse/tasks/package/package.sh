c3a0332da4e389495b17c67c2fd4a802a995def9
bruno.catarino <bruno.catarino@sainsburys.co.uk>
2019-02-14 13:57:15 +0000

    some prints to evaluate what's in the version folder

--- concourse/tasks/package/package.sh
+++ concourse/tasks/package/package.sh
@@ -3,20 +3,16 @@
 REPO_DIR=$(pwd)/$1
 BUILD_DIR=$(pwd)/$2
 
-VERSION=$3
-
 echo $(pwd)
+ls $(pwd)
+
+VERSION=$(pwd)/$3
+
+ls $VERSION
+
 echo ${REPO_DIR}
 echo ${BUILD_DIR}
 
-#echo "setting aws keys"
-#export AWS_ACCESS_KEY_ID=$3
-#export AWS_SECRET_ACCESS_KEY=$4
-#
-#echo ${AWS_ACCESS_KEY_ID}
-#echo ${AWS_SECRET_ACCESS_KEY}
-
-# TODO remove skipTests once the integration tests are sorted
 mvn -f product-blacklist-repo/pom.xml clean package -X -e
 
 if [ ! -f product-blacklist-repo/target/*.jar ]; then
@@ -24,18 +20,6 @@ if [ ! -f product-blacklist-repo/target/*.jar ]; then
     exit -1
 fi
 
-echo "list repo dir"
-ls ${REPO_DIR}
-
-echo "list repo target dir"
-ls ${REPO_DIR}/target
-
-echo "list build dir before copying"
-ls -las ${BUILD_DIR}
-
 echo "copying files to output directory"
 cp ${REPO_DIR}/target/*.jar ${BUILD_DIR}
 cp ${REPO_DIR}/concourse/assets/manifest*.yml ${BUILD_DIR}
-
-echo "list build dir after copying"
-ls -las ${BUILD_DIR}
