c720777e8719e0fa0b889dc4d47f25103e7fa37b
bruno.catarino <bruno.catarino@sainsburys.co.uk>
2019-02-18 12:48:52 +0000

    Parametrise manifest folder

--- concourse/tasks/package/package.sh
+++ concourse/tasks/package/package.sh
@@ -2,11 +2,12 @@
 
 REPO_DIR=$(pwd)/$1
 BUILD_DIR=$(pwd)/$2
+MANIFEST_DIR=$(pwd)/$3
 
 echo $(pwd)
 ls $(pwd)
 
-ARTIFACT_NAME=$3
+ARTIFACT_NAME=$4
 
 echo ${REPO_DIR}
 echo ${BUILD_DIR}
@@ -22,7 +23,7 @@ DEST_NAME="$ARTIFACT_NAME.jar"
 
 echo "copying files to output directory"
 cp ${REPO_DIR}/target/*.jar ${BUILD_DIR}/$DEST_NAME
-cp ${REPO_DIR}/concourse/assets/manifest*.yml ${BUILD_DIR}
+cp ${MANIFEST_DIR}/manifest*.yml ${BUILD_DIR}
 
 echo "Final files"
 ls ${BUILD_DIR}
